10df9cc96a311a5ecd2bab77f0f61b24ba75370d
imran <87297004+imranshaikh9930@users.noreply.github.com>
2025-03-20 15:45:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+// Import stylesheets
+
+// Get input elements
+const nameVal = document.getElementById("name");
+const emailVal = document.getElementById("email");
+const passwordVal = document.getElementById("password");
+
+const validateForm = () => {
+    
+    if (/^[A-Za-z]{3,}$/.test(nameVal.value)) { 
+        nameVal.style.backgroundColor = "lightgreen";  
+    } else {
+        nameVal.style.backgroundColor = "lightcoral";  
+    }
+
+    
+    if (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(emailVal.value)) {
+        emailVal.style.backgroundColor = "lightgreen"; 
+    } else {
+        emailVal.style.backgroundColor = "lightcoral"; 
+    }
+
+   
+    if (/[0-9]{10}/.test(passwordVal.value)) {
+        passwordVal.style.backgroundColor = "lightgreen"; 
+    } else {
+        passwordVal.style.backgroundColor = "lightcoral"; 
+    }
+};
+
+
+nameVal.addEventListener("input", validateForm);
+emailVal.addEventListener("input", validateForm);
+passwordVal.addEventListener("input", validateForm);
